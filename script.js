67280e3895f3263ffe5e494897a53d36ab281159
Tobias Rupp <tobias.rupp@gmx.com>
2015-02-08 23:34:12 +0000

    Added new JQuery

--- script.js
+++ script.js
@@ -1,82 +1,126 @@
-$(document).ready(function()
-{
-    $('.flower1').draggable();
+// $(document).ready(function()
+// {
+//     $('.flower1').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $(".flower2").draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower3').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower4').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower5').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower6').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower7').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower8').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.flower9').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.leaf').draggable();
+// });
+
+// $(document).ready(function()
+// {
+//     $('.leaf2').draggable();
+// });
+
+// $(document).ready(function() {
+//    $('.leaf2').mouseenter(function() {
+//        $(this).animate({
+//            height: '+=10px'
+//        });
+//    });
+//    $('.leaf2').mouseleave(function() {
+//        $(this).animate({
+//            height: '-=10px'
+//        }); 
+//    });
+//    $('.leaf2').click(function() {
+//        $(this).toggle(1000);
+//    }); 
+// });
+
+
+// $(document).ready(function()
+// {
+//     $(".content").hover(function()
+//     {
+//         $(".flowers").fadeOut('slow', function ()
+//         {
+//             $(".flowers").fadeIn('slow');
+//         });
+//     });
+// });
+
+$(document).ready(function(){
+    $('.flower1').animate({top:'+=250px'},1000);
 });
 
-$(document).ready(function()
-{
-    $(".flower2").draggable();
+$(document).ready(function(){
+    $('.flower2').animate({top:'+=250px'},2000);
 });
 
-$(document).ready(function()
-{
-    $('.flower3').draggable();
+$(document).ready(function(){
+    $('.flower3').animate({top:'+=250px'},1000);
 });
 
-$(document).ready(function()
-{
-    $('.flower4').draggable();
+$(document).ready(function(){
+    $('.flower4').animate({top:'+=220px'},2000);
 });
 
-$(document).ready(function()
-{
-    $('.flower5').draggable();
+$(document).ready(function(){
+    $('.flower5').animate({top:'+=240px'},1000);
 });
 
-$(document).ready(function()
-{
-    $('.flower6').draggable();
+$(document).ready(function(){
+    $('.flower6').animate({top:'+=200px'},2300);
 });
 
-$(document).ready(function()
-{
-    $('.flower7').draggable();
+$(document).ready(function(){
+    $('.flower7').animate({top:'+=230px'},2200);
 });
 
-$(document).ready(function()
-{
-    $('.flower8').draggable();
+$(document).ready(function(){
+    $('.flower8').animate({top:'+=260px'},1000);
 });
 
-$(document).ready(function()
-{
-    $('.flower9').draggable();
+$(document).ready(function(){
+    $('.flower9').animate({top:'+=200px'},1000);
 });
 
-$(document).ready(function()
-{
-    $('.leaf').draggable();
+$(document).ready(function(){
+    $('.leaf').animate({top:'+=260px'},2100);
 });
 
-$(document).ready(function()
-{
-    $('.leaf2').draggable();
-});
-
-$(document).ready(function() {
-   $('.leaf2').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('.leaf2').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('.leaf2').click(function() {
-       $(this).toggle(1000);
-   }); 
-});
-
-
-$(document).ready(function()
-{
-    $(".content").hover(function()
-    {
-        $(".flowers").fadeOut('slow', function ()
-        {
-            $(".flowers").fadeIn('slow');
-        });
-    });
+$(document).ready(function(){
+    $('.leaf2').animate({top:'+=240px'},1000);
 });
